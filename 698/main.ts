958203f02c1b995abb4a70010fc9da138add4e6a
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-05 08:51:13 +0200

    698: Use map() instead of replaceAll()

--- 698/main.ts
+++ 698/main.ts
@@ -1,3 +1,3 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_698.md
 
-export default (text: string): string => text.toLowerCase().split(" ").filter(word => word === "zero" || word === "one").filter((_,i,array) => array.length - i > array.length % 8).join("_").replaceAll("zero", "0").replaceAll("one", "1").replaceAll("_", "");
+export default (text: string): string => text.toLowerCase().split(" ").filter(word => word === "zero" || word === "one").filter((_,i,array) => array.length - i > array.length % 8).map(word => word === "zero" ? "0" : "1").join("");
