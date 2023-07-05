518b31c1e0bdc4ca7f6c479aff0abb549d3a8815
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-05 08:59:13 +0200

    691: use map instead of forEach

--- 691/main.ts
+++ 691/main.ts
@@ -1,13 +1,4 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_691.md
 
-export default (numbers: number[]): number[] => {
-    let output: number[] = [];
-    numbers.forEach(
-        (_, i) =>
-            (output[i] = numbers.reduce(
-                (sum, value, index) => (index !== i ? sum * value : sum),
-                1,
-            )),
-    );
-    return output;
-};
+export default (numbers: number[]): number[] => numbers.map((_, i) => numbers.reduce((sum, value, index) => (index !== i ? sum * value : sum), 1));
+
