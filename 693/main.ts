761f549f1d8e1d3c6c2ad7b4e0cf94deeb5bc9e7
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-05 08:56:36 +0200

    693: map instead of forEach

--- 693/main.ts
+++ 693/main.ts
@@ -1,12 +1,3 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_693.md
 
-export default (sentence: string): string => {
-    let output: string[] = [];
-    sentence.split(" ").forEach((word) => {
-        word = word.replace(/^x\B/, "z"); // Starts with x
-        word = word.replace(/^x\b/, "ecks"); // Word is only x
-        word = word.replace(/x/g, "cks"); // Other cases
-        output.push(word);
-    });
-    return output.join(" ");
-};
+export default (sentence: string): string => sentence.split(" ").map(word => word.replace(/^x\B/, "z").replace(/^x\b/, "ecks").replace(/x/g, "cks")).join(" ");
