33f7b51dd7a372bf32a679cce670a48779539cd0
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-05 08:35:22 +0200

    Challenge 698: remove some unneeded code

--- 698/main.ts
+++ 698/main.ts
@@ -1,3 +1,3 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_698.md
 
-export default (text: string): string => text.toLowerCase().split(" ").filter(word => word.toLowerCase() === "zero" || word.toLowerCase() === "one").filter((_,i,array) => array.length - i > array.length % 8).join("_").replaceAll("zero", "0").replaceAll("one", "1").replaceAll("_", "");
+export default (text: string): string => text.toLowerCase().split(" ").filter(word => word === "zero" || word === "one").filter((_,i,array) => array.length - i > array.length % 8).join("_").replaceAll("zero", "0").replaceAll("one", "1").replaceAll("_", "");
