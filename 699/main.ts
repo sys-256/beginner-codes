4fb6ec1d89e67785124bd5879f1264bcc68595f3
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-06 15:27:38 +0200

    Challenge 699

---
+++ 699/main.ts
@@ -0,0 +1,3 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_699.md
+
+export default (first_month: number, multiplier: number): number => Math.ceil((Math.log(1000000 * (multiplier - 1) + first_month) - Math.log(first_month)) / Math.log(multiplier));
