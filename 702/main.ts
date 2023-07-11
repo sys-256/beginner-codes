d327ac7ef3b472b1e1656b42b7ef60842b6f7006
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-11 12:27:49 +0200

    Challenge 702

---
+++ 702/main.ts
@@ -0,0 +1,3 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_702.md
+
+export default (code: string): string[] | string => !/^\d{4}$/.test(code) ? "Invalid input." : code.split("").map((value, i) => ["Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado", "Pop", "Lock", "Arabesque"][Number(value) + i - (Number(value) + i <= 9 ? 0 : 10)]);
