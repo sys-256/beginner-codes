61f03613a81bcdab0aadbba718e1532c969d9325
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-12 12:38:06 +0200

    Challenge 703

---
+++ 703/main.ts
@@ -0,0 +1,3 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_703.md
+
+export default (time1: string, time2: string): string => time1 === time2 ? "no time passed" : `${Number(time2.split(":")[0]) + (time2.split(" ")[1] === "PM" ? 12 : 0) - Number(time1.split(":")[0]) - (time1.split(" ")[1] === "PM" ? 12 : 0)} hours`;
