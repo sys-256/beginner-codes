918ae94ee3742678577c0adcf2d18bb1150de332
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-20 11:19:51 +0200

    Challenge 709

---
+++ 709/main.ts
@@ -0,0 +1,3 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_709.md
+
+export default (question_times: number[], total_time: number): string => total_time <= 120 && question_times.length === 8 && question_times.every((value, i) => value <= [5, 5, 10, 10, 15, 15, 20, 20][i]) ? `qualified` : `disqualified`;
