fe0994dde2a23ba211ded6fa01467467e9db3b1b
Bram <84628826+sys-256@users.noreply.github.com>
2023-07-21 07:30:53 +0200

    Challenge 710

---
+++ 710/main.ts
@@ -0,0 +1,16 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_710.md
+
+export default (iterations: number): number => {
+    let total = 0,
+    	stars = 1;
+    for (let i = 1; i < iterations; ++i) {
+        total += stars;
+        stars += 2;
+    }
+
+    for (let i = stars; i >= iterations; --i) {
+        total += i;
+    }
+
+    return total;
+};
