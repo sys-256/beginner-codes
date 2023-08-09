228e85e28eafeb0b3cc947a3a933754a7e6072a3
Bram <84628826+sys-256@users.noreply.github.com>
2023-08-09 13:05:42 +0200

    723: improvement

--- 723/main.ts
+++ 723/main.ts
@@ -1,3 +1,3 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_723.md
 
-export default (n: number[]): number[] => n.map((_, i) => n.slice().splice(0, i + 1).reduce((a, b) => a + b));
+export default (n: number[]): number[] => n.map((_, i) => n.slice(0, i + 1).reduce((a, b) => a + b));
