832cd40e17c1f1a9439d6b322932276c5932ff53
Bram <84628826+sys-256@users.noreply.github.com>
2023-08-15 12:24:06 +0200

    Challenge 727

---
+++ 727/main.ts
@@ -0,0 +1,14 @@
+// https://github.com/beginnerpy-com/challenges/blob/main/weekday/challenge_727.md
+
+export default (message: string): string =>
+    message
+        .split(``)
+        .map(
+            x =>
+                `0`.repeat(8 - x.charCodeAt(0).toString(2).length) +
+                x.charCodeAt(0).toString(2),
+        )
+        .join(``)
+        .split(``)
+        .map(x => x.repeat(3))
+        .join(``);
