1ee5a17c831c44ae1f93b88f05a0d6c948e329cb
Bram <84628826+sys-256@users.noreply.github.com>
2023-08-22 12:16:23 +0200

    Challenge 732

---
+++ 732/main.ts
@@ -0,0 +1,13 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_732.md
+
+export default (binary: string): string =>
+	(binary.match(/.{8}/g) ?? ([] as string[]))
+		.map(x =>
+			String.fromCharCode(
+				x
+					.split(``)
+					.map(Number)
+					.reduce((a, b, i) => a + b * 2 ** (7 - i))
+			)
+		)
+		.join(``);
