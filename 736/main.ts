d84d9ad135f17743163305c5ac94f67f205b339d
Bram <84628826+sys-256@users.noreply.github.com>
2023-08-27 12:57:40 +0200

    736: Slight performance improvement

--- 736/main.ts
+++ 736/main.ts
@@ -1,8 +1,8 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_736.md
 
 export default (n: number[]) => {
-	for (let i = 2; i < n.length; ++i) {
-		if (n.length % i === 0 && new Array(n.length / i).fill(n.slice(0, i)).flat().toString() === n.toString())
+	for (let i = 2; i < n.length / 2 + 1; ++i) {
+		if (!(n.length % i) && new Array(n.length / i).fill(n.slice(0, i)).flat().toString() === n.toString())
 			return true;
 	}
 
