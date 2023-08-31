96a524d0a50cb795eca8939aa031a83548e8dfb1
Bram <84628826+sys-256@users.noreply.github.com>
2023-08-31 10:02:19 +0200

    Challenge 740

--- 740/main.ts
+++ 740/main.ts
@@ -1,3 +1,7 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_740.md
 
-export default () => {}
+export default (n: number) =>
+	new Array(n)
+		.fill(0)
+		.map((_, i) => (n * (n - 1)) / 2 + 1 + i)
+		.reduce((a, x) => a + x);
