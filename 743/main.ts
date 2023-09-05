1536f4b4402b5ad0f9de6572050152e7d4b6d9ee
Bram <84628826+sys-256@users.noreply.github.com>
2023-09-05 18:31:30 +0200

    Challenge 743

---
+++ 743/main.ts
@@ -0,0 +1,7 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_743.md
+
+export default (w: string[]): string[] => {
+	const dict: { [index: string]: number } = {};
+	w.join(``).split(``).forEach(x => dict[x]++ || (dict[x] = 1));
+	return Object.keys(dict).filter(x => dict[x] === Math.max(...Object.values(dict))).sort();
+};
