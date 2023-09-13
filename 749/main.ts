23ce38651a21b3c7539a2e962b7571503dfeb6c9
Bram <84628826+sys-256@users.noreply.github.com>
2023-09-13 09:11:48 +0200

    Challenge 749

---
+++ 749/main.ts
@@ -0,0 +1,3 @@
+// https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_749.md
+
+export default (l1: number[], l2: number[]) => !!l1 && !!l2 && l1.map(x => x * x).sort().toString() === l2.sort().toString();
