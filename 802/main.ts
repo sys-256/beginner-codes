08fdc1cdbedc42314621d813a0169daed0afd383
Bram <84628826+sys-256@users.noreply.github.com>
2023-11-30 17:07:47 +0100

    Improve 802

--- 802/main.ts
+++ 802/main.ts
@@ -1,13 +1,7 @@
 // https://github.com/beginner-codes/challenges/blob/main/weekday/challenge_802.md
 
 export default (n: number) => {
-	let c = n,
-		s = [c];
-
-	while (c !== 1) {
-		c = !(c % 2) ? c / 2 : 3 * c + 1;
-		s.push(c);
-	}
-
+	let s = [n];
+	while (n !== 1) s.push((n = n % 2 ? 3 * n + 1 : n / 2));
 	return [s.length, Math.max(...s)];
 };
